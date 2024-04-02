915850b6185f68c2a5ea8c8d94cf535bd3737d1c
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-04-01 23:12:54 -0300

    ✨ feat: update main.ts with new imports and configurations

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+import Toast from 'vue-toastification'
 import App from './App.vue'
+import router from './routes'
+import { ToastOptions } from '~/configs/toast'
 
-createApp(App).mount('#app')
+import '~/assets/styles/index.css'
+import 'vue-toastification/dist/index.css'
+
+const pinia = createPinia()
+
+createApp(App)
+  .use(pinia)
+  .use(router)
+  .use(Toast, ToastOptions)
+  .mount('#app')
