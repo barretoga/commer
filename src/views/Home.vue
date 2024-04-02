f65431b2acb95e39bd2a2af75408dc0e5b457eda
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-04-01 23:14:42 -0300

    ✨ feat: add Home.vue component with Navigation

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Navigation from '~/blocks/Navigation.vue';
+import { menuItems } from '~/static/menu'
+
+</script>
+
+<template>
+  <div class="grid justify-center">
+    <Navigation
+      :menuItems
+    />
+  </div>
+</template>
