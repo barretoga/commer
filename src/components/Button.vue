d96f20bf52655b7b2ebb2138362f720e38ba225a
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-04-06 15:32:19 -0300

    ♻️ refact: Button.vue component to use classType prop instead of returnBtnClass computed property

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,27 +1,19 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-
 interface Props {
   type?: 'submit' | 'button' | 'reset'
-  classType?: 'primary' | 'secondary' | 'neutral' | 'accent' | 'ghost' | 'link'
 }
 
-const props = withDefaults(defineProps<Props>(), {
+withDefaults(defineProps<Props>(), {
   type: 'button',
   classType: 'primary'
 })
 
 const emit = defineEmits(['onClick'])
-
-const returnBtnClass = computed(() => {
-  return `btn-${props.classType}`
-})
 </script>
 
 <template>
   <button
-    class="btn"
-    :class="returnBtnClass"
+    class="btn btn-primary"
     :type
     @click="emit('onClick')"
   >
