033c5d506571a70561ad2f54d3578731a96001f6
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-04-06 15:32:49 -0300

    ✨ feat: add Footer, Navigation, and menuItems to BlankLayout.vue

--- src/layouts/BlankLayout.vue
+++ src/layouts/BlankLayout.vue
@@ -1,8 +1,25 @@
 <script setup lang="ts">
+import Footer from '~/blocks/Footer.vue'
+import Navigation from '~/blocks/Navigation.vue';
+import { menuItems } from '~/static/menu'
+
+
 </script>
 
 <template>
-  <div>
+  <div class="grid mx-auto max-w-[1200px]">
+    <div class="flex justify-between mt-5">
+      <div>
+
+      </div>
+      <Navigation
+        :menuItems="menuItems"
+      />
+      <div>
+
+      </div>
+    </div>
     <RouterView />
+    <Footer />
   </div>
 </template>
