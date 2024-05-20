35efae02e773df0e8d45b4c10fa901effbe14501
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-05-20 20:25:10 -0300

    ✨ feat: Add clothSizes and footSizes arrays to product-info.ts

---
+++ src/static/product-info.ts
@@ -0,0 +1,19 @@
+export const clothSizes = [
+  'PP',
+  'P',
+  'M',
+  'G',
+  'GG',
+  'XGG'
+]
+
+export const footSizes = [
+  '34',
+  '35',
+  '36',
+  '37',
+  '38',
+  '39',
+  '40',
+  '41',
+]
