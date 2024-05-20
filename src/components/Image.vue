3d3646096cc72c5eaab89bc60bc64cb10f27efda
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-05-20 20:25:39 -0300

    ✨ feat: Add Image component for lazy loading images

---
+++ src/components/Image.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { useImage } from '@vueuse/core'
+
+interface Props {
+  src: string
+  alt?: string
+  width?: number
+  height?: number
+  lazyLoad?: 'eager' | 'lazy'
+  title?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  lazyLoad: 'lazy',
+})
+
+const { isLoading } = useImage({
+  src: props.src ?? '',
+})
+</script>
+
+<template>
+  <span v-if="isLoading">
+    Loading
+  </span>
+  <img
+    v-else
+    v-bind="$attrs"
+    :alt
+    :title
+    :src
+    :loading="lazyLoad"
+    :height
+    :width
+    class="h-auto w-full"
+  >
+</template>
