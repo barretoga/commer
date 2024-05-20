f1303b8b26cf28ea1c52d34949c5056938038669
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-05-20 20:45:40 -0300

    ✨ feat: Add Form and InputText components

---
+++ src/components/Form.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface Props {
+  modelValue: Object
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <Formkit
+    v-model="modelValue"
+    :actions="false"
+    type="form"
+  >
+    <slot />
+  </Formkit>
+</template>
---
+++ src/components/InputText.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+interface Props {
+  name: string
+  label: string
+  placeholder?: string
+  validation?: string
+  disabled?: boolean
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <Formkit
+    :name
+    :label
+    :placeholder
+    :validation
+    :disabled
+    type="text"
+  />
+</template>
