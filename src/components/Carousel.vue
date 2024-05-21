04a1c47d37155dd5df144be03a9b0b6508679c74
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-05-21 20:17:02 -0300

    ✨ feat: Add Carousel component for image slideshow

---
+++ src/components/Carousel.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { Navigation, Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+interface Props {
+  slidesPerView: number
+  spaceBetween: string
+  slides: string[]
+}
+
+defineProps<Props>()
+
+const modules = [Autoplay, Navigation]
+
+function onSwiper(swiper: any) {
+  console.log(swiper)
+}
+
+function onSlideChange() {
+  console.log('slide change')
+}
+</script>
+
+<template>
+  <swiper
+    :slides-per-view
+    :space-between
+    :autoplay="{
+      delay: 3500,
+      disableOnInteraction: false,
+    }"
+    :modules
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+  >
+    <swiper-slide
+      v-for="(slide, index) in slides"
+      :key="index"
+    >
+      <Image
+        :src="slide"
+        class="w-full h-full"
+      />
+    </swiper-slide>
+  </swiper>
+</template>
