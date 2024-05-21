3b045c208f994ec39bc419576b89c10ae1f34a4c
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-05-21 20:44:50 -0300

    ✨ feat: Update BlankLayout.vue to display user profile image and name in Navigation

--- src/layouts/BlankLayout.vue
+++ src/layouts/BlankLayout.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 import { menuItems } from '~/static/menu'
 import Footer from '~/blocks/Footer.vue'
 import Navigation from '~/blocks/Navigation.vue';
 
+const showUsername = ref(false)
 const user = reactive({
   image: 'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg',
   name: 'Gabriel Barreto'
@@ -16,13 +17,20 @@ const user = reactive({
       <Navigation
         :menuItems="menuItems"
       />
-      <div class="flex items-center gap-x-2">
-        <span>
+      <div
+        class="flex items-center justify-center gap-x-2 bg-base-200 p-2 rounded-2xl hover:cursor-pointer"
+        @mouseleave="showUsername = false"
+      >
+        <span
+          v-if="showUsername"
+          class="animate-slide-in-right"
+        >
           {{ user.name }}
         </span>
         <Avatar
           :image="user.image"
-          class="w-[3rem] border-primary border-2"
+          class="w-[2.2rem] border-primary border-2"
+          @mouseover="showUsername = true"
         />
       </div>
     </div>
