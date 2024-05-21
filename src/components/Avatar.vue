c4afaa61ccc3bd6d7daa3fddd61b229efce69d2f
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-05-21 20:16:55 -0300

    ✨ feat: Add Avatar component for displaying user profile images

---
+++ src/components/Avatar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+  image: string
+  class: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="avatar">
+    <div
+      :class
+      class="w-24 rounded-xl"
+    >
+      <Image
+        :src="image"
+      />
+    </div>
+  </div>
+</template>
