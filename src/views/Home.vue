897ef4f465a24e69c160605adbe9cd2194ed1942
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-05-21 20:17:42 -0300

    ✨ feat: Add lazy loading for images and Carousel component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 
 const product = reactive({
   name: "t-shirt",
@@ -18,9 +18,22 @@ const product = reactive({
     availableSizes: ['M', 'P', 'G']
   }
 })
+
+const seasons = ref([
+  'https://s3.wasabisys.com/instax/74/instax/2022/08/fotografias-de-paisagens-1661176157.jpeg',
+  'https://images.memphistours.com/large/1384dab11b121931eba246f2ef0a42ec.jpg',
+  'https://s2-casavogue.glbimg.com/9lsEga4y46bwBpZeTvf3woNB2k4=/0x0:2121x1414/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_d72fd4bf0af74c0c89d27a5a226dbbf8/internal_photos/bs/2023/z/m/php5y6Qieb0OBeoZhDeg/01-as-paisagens-naturais-mais-lindas-do-brasil-floresta-amazonica-min.jpg'
+])
 </script>
 
 <template>
+  <div class="max-h-[18rem] rounded-lg w-full mx-auto overflow-hidden mb-10">
+    <Carousel
+      :slides="seasons"
+      :slides-per-view="1"
+      space-between="30"
+    />
+  </div>
   <div class="flex items-center">
     <Icon
       icon="fxemoji:fire"
@@ -39,4 +52,7 @@ const product = reactive({
       :product="product"
     />
   </div>
+  <div
+    class="divider my-5"
+  />
 </template>
