e8e502d725b6c4fb13364cbcc64772515722f42e
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-07-13 18:04:00 -0300

    ✨ feat: create Image component

---
+++ src/components/Image.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface Props {
+  src: string
+  alt?: string
+  title?: string
+  lazyLoad?: 'eager' | 'lazy'
+  width?: number
+  height?: number
+}
+
+withDefaults(defineProps<Props>(), {
+  lazyLoad: 'lazy',
+})
+</script>
+
+<template>
+  <img
+    :loading="lazyLoad"
+    :alt
+    :title
+    :src
+    class="w-full"
+  >
+</template>
