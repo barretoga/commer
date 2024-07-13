975aad91a6bfbd719b2d7664a07cd1895f6979c2
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-07-13 18:04:35 -0300

    ✨ feat: create settings component

---
+++ src/blocks/Settings.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import Icon from '@/components/Icon.vue';
+
+const emit = defineEmits(['toggleShoppingCart', 'toggleNotifications'])
+</script>
+
+<template>
+  <div class="flex items-center gap-10 mr-4">
+    <button
+      type="button"
+      @click="emit('toggleNotifications')"
+    >
+      <Icon
+        class="hover:text-slate-600 transition-all duration-300"
+        width="1.5em"
+        icon="bx:bell"
+      />
+    </button>
+    <a
+      href="/favorites"
+    >
+      <Icon
+        class="hover:cursor-pointer hover:text-slate-600 transition-all duration-300"
+        width="1.5em"
+        icon="bx:heart"
+      />
+    </a>
+    <button
+      type="button"
+      @click="emit('toggleShoppingCart')"
+    >
+      <Icon
+        class="hover:text-slate-600 transition-all duration-300"
+        width="1.5em"
+        icon="bx:shopping-bag"
+      />
+    </button>
+  </div>
+</template>
