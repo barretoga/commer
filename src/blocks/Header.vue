a7e8cc68963b604f3eda3e9a2638c6f74d45fdc1
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-07-13 18:02:43 -0300

    ✨ feat: create header block

---
+++ src/blocks/Header.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Navigation from '@/components/Navigation.vue';
+import { navigationItems } from '@/static/navigation';
+import Settings from '@/blocks/Settings.vue';
+import Image from '@/components/Image.vue';
+
+const emit = defineEmits(['toggleShoppingCart', 'toggleNotifications'])
+</script>
+
+<template>
+  <div class="flex justify-between gap-4 items-center mx-auto w-full pr-6">
+    <Image
+      class="my-auto object-center mt-1 object-scale-down w-[170px] h-[100px]"
+      src="/logo-white.png"
+    />
+    <Navigation
+      :items="navigationItems"
+      class="md:block hidden"
+    />
+    <Settings
+      @toggle-notifications="emit('toggleNotifications')"
+      @toggle-shopping-cart="emit('toggleShoppingCart')"
+    />
+  </div>
+</template>
