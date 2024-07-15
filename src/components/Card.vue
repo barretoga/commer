c352af2d1067c78823d0a6b982bc1c825dc68ba9
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-07-14 21:15:40 -0300

    ✨ feat: create card component

---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import {
+  Card as CnCard,
+  CardContent
+} from '@/components/ui/card'
+</script>
+
+<template>
+  <CnCard>
+    <CardContent
+      class="flex aspect-square items-center justify-center"
+    >
+      <slot />
+    </CardContent>
+  </CnCard>
+</template>
