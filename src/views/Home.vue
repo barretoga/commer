715a61586c98269c8f6d127d531a1f0e7e4127b9
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-07-14 21:23:48 -0300

    ✨ feat: improve home page with carousel and products

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
+import Carousel from '@/components/Carousel.vue';
+import { highlights, products, tabs } from '@/static/home';
+import Products from '@/views/Home/Products.vue';
+
 </script>
 
 <template>
-  <div class="bg-red-400">
-    Home
+  <div class="flex justify-center w-full">
+    <Carousel
+      :items="highlights"
+      class="w-full md:min-w-[500px] lg:min-w-[800px] xl:min-w-[1000px]"
+    />
+  </div>
+  <div
+    class="w-full md:min-w-[500px] lg:min-w-[800px] xl:max-w-[1000px] mx-auto"
+  >
+    <Products
+      :tabs="tabs"
+      :products="products"
+    />
   </div>
 </template>
