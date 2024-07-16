8dacc6843fb873c74ca36e49dc157bd0e9ad3932
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-07-16 20:58:19 -0300

    ♻️ refact: add side menu and footer to blank layout

--- src/layouts/BlankLayout.vue
+++ src/layouts/BlankLayout.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
+import Footer from '@/blocks/Footer.vue';
 import Header from '@/blocks/Header.vue';
+import SideMenu from '@/blocks/SideMenu.vue';
+import { ref } from 'vue';
+import { navigationItems } from '@/static/navigation'
+
+const menuVisibility = ref(false);
 
 function toggleNotifications() {
   console.log('notifications')
@@ -11,13 +17,24 @@ function toggleShoppingCart() {
 </script>
 
 <template>
-  <div>
+  <div
+    :class="{ 'overflow-hidden max-h-[70%]': menuVisibility }"
+  >
     <Header
+      :menu-visibility="menuVisibility"
       @toggle-notifications="toggleNotifications"
       @toggle-shopping-cart="toggleShoppingCart"
+      @toggle-menu-visibility="menuVisibility = !menuVisibility"
     />
-    <main class="mx-auto xl:container h-full">
+    <main
+      class="flex mx-auto xl:container h-full mt-[7rem]"
+    >
+      <SideMenu
+        :menu-visibility
+        :menu-items="navigationItems"
+      />
       <RouterView />
     </main>
+    <Footer/>
   </div>
 </template>
