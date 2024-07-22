589c5a5f417c58d56b727a2f9c8dac1bd27579bd
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-07-21 21:36:19 -0300

    ✨ feat: add pinia configuration to main ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 import router from '@/routes'
 
 import './assets/index.css'
 
+const pinia = createPinia()
+
 createApp(App)
   .use(router)
+  .use(pinia)
   .mount('#app')
