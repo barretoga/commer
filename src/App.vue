c87d32b5b91ebc122da2643506052c9023a39ee1
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-08-01 23:10:10 -0300

    🐛 fix: remove default css and add min height

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,7 @@
 </script>
 
 <template>
-  <div class="flex flex-col h-[100vh]">
+  <div class="flex flex-col min-h-[100vh]">
     <RouterView />
   </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
