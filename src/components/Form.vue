965fe3575f06b2d5ce0ab523e5b6acbeda3125c8
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-08-01 23:12:52 -0300

    ✨ feat: create form component

---
+++ src/components/Form.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useForm } from 'vee-validate'
+
+const emit = defineEmits(['submit'])
+const form = useForm()
+
+const onSubmit = form.handleSubmit((values) => {
+  emit('submit', values)
+})
+</script>
+
+<template>
+  <form @submit="onSubmit">
+    <slot />
+  </form>
+</template>
