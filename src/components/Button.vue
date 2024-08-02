9ba0716248109d84786a6b73a1efb07fe0e7facb
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2024-08-01 23:37:55 -0300

    ✨ feat: create button component

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+
+interface Props {
+  title?: string
+  type?: 'submit' | 'button' | 'reset'
+}
+
+withDefaults(defineProps<Props>(),{
+  type: 'submit'
+})
+const emit = defineEmits(['onClick'])
+</script>
+
+<template>
+  <Button
+    :type
+    :title
+    @click="emit('onClick')"
+  >
+    <slot />
+  </Button>
+</template>
